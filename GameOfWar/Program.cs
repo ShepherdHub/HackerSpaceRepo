f2cbe721dc81ccfb819915585ba6645b98d0e5bb
Steven Shepherd <steven.shepherd814@gmail.com>
2014-02-08 16:24:03 -0500

    Adding csharp version of game of war

---
+++ GameOfWar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameOfWar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String input = Console.ReadLine();
+
+        }
+    }
+}
